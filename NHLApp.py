6f4ea407071900b3d5ad6db8bec07ac5db132132
Ryan Joseph <160825414+rj7002@users.noreply.github.com>
2025-07-18 01:55:17 -0700

    Update NHLApp.py

--- NHLApp.py
+++ NHLApp.py
@@ -250,6 +250,7 @@ if season:
         period_options = df['period'].unique()
         selected_period = st.sidebar.multiselect("Select Period", period_options,default=period_options.tolist())
         df = df[df['period'].isin(selected_period)]
+        xGoal = st.sidebar.checkbox('xG')
         if st.sidebar.toggle('Filter by players'):
             if filter == 'Shooter':
                 player_options = df['goalieNameForShot'].unique()
@@ -466,16 +467,28 @@ if season:
         if rinktype == 'Half':
             with col1:
                 fig = go.Figure()
-                fig.add_trace(go.Scatter(
-                    y=-df['xCordAdjusted'],
-                    x=-df['yCordAdjusted'],
-                    mode='markers',
-                    marker=dict(color=df['color'], size=10,opacity=df['xG'],symbol=df['symbol']),
-                    showlegend=False,
-                    name='End Points',
-                    hovertext=hoverlabel,
-                        hoverinfo='text',
-                ))
+                if xGoal:
+                    fig.add_trace(go.Scatter(
+                        y=-df['xCordAdjusted'],
+                        x=-df['yCordAdjusted'],
+                        mode='markers',
+                        marker=dict(color=df['color'], size=10,opacity=df['xG'],symbol=df['symbol']),
+                        showlegend=False,
+                        name='End Points',
+                        hovertext=hoverlabel,
+                            hoverinfo='text',
+                    ))
+                else:
+                    fig.add_trace(go.Scatter(
+                        y=-df['xCordAdjusted'],
+                        x=-df['yCordAdjusted'],
+                        mode='markers',
+                        marker=dict(color=df['color'], size=10,opacity=0.5,symbol=df['symbol']),
+                        showlegend=False,
+                        name='End Points',
+                        hovertext=hoverlabel,
+                            hoverinfo='text',
+                    ))
                 create_hockey_rink(fig,setting='dzone',vertical=True)
                 data = st.plotly_chart(fig,on_select='rerun')
                 x = data["selection"]["points"][0]["x"]
@@ -575,16 +588,28 @@ if season:
                 st.plotly_chart(fig)
             with col1:
                 fig = go.Figure()
-                fig.add_trace(go.Scatter(
-                    x=df['xCord'],
-                    y=df['yCord'],
-                    mode='markers',
-                    marker=dict(color=df['color'], size=10,opacity=df['xG'],symbol=df['symbol']),
-                    name='End Points',
-                    hovertext=hoverlabel,
-                    showlegend=False,
-                        hoverinfo='text',
-                ))
+                if xGoal:
+                    fig.add_trace(go.Scatter(
+                        y=-df['xCordAdjusted'],
+                        x=-df['yCordAdjusted'],
+                        mode='markers',
+                        marker=dict(color=df['color'], size=10,opacity=df['xG'],symbol=df['symbol']),
+                        showlegend=False,
+                        name='End Points',
+                        hovertext=hoverlabel,
+                            hoverinfo='text',
+                    ))
+                else:
+                    fig.add_trace(go.Scatter(
+                        y=-df['xCordAdjusted'],
+                        x=-df['yCordAdjusted'],
+                        mode='markers',
+                        marker=dict(color=df['color'], size=10,opacity=0.5,symbol=df['symbol']),
+                        showlegend=False,
+                        name='End Points',
+                        hovertext=hoverlabel,
+                            hoverinfo='text',
+                    ))
                 create_hockey_rink(fig,setting='full',vertical=False)
                 data = st.plotly_chart(fig,on_select='rerun')
                 if data and 'selection' in data and 'points' in data['selection'] and len(data['selection']['points']) > 0:
